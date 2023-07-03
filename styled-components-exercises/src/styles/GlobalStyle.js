6835d94f2152cc64df4dad9d9358bdbc40ecee62
Thomas Lincoln <thmas5952@gmail.com>
2023-07-03 12:37:50 -0300

    Base App

---
+++ styled-components-exercises/src/styles/GlobalStyle.js
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body{
+    margin: 0;
+    padding: 0;
+    display: inline-block;
+    background-color: darkcyan;
+    font-family: Arial, Helvetica, sans-serif;
+  }
+`;
+
+export default GlobalStyle;
