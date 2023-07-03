93ce1c2b4a5e27ed4a440e36eb539b3c62b97a4d
Thomas Lincoln <thmas5952@gmail.com>
2023-07-03 12:50:16 -0300

    Exercise one

--- styled-components-exercises/src/App.js
+++ styled-components-exercises/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import GlobalStyle from './styles/GlobalStyle';
+import Botao from './styles/exercicio1';
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
+      <Botao>Start</Botao>
       <p />
     </div>
   );
---
+++ styled-components-exercises/src/styles/exercicio1.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Botao = styled.button`
+  background-color: blue;
+  color: white;
+  margin: 20px;
+  border: none;
+  height: 40px;
+  padding: 10px;
+`;
+
+export default Botao;
