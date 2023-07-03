6ae38b137615c4aeaa9cb750afad775ea339a6c3
Thomas Lincoln <thmas5952@gmail.com>
2023-07-03 13:27:53 -0300

    exercicio 2

--- styled-components-exercises/src/App.js
+++ styled-components-exercises/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import GlobalStyle from './styles/GlobalStyle';
 import Botao from './styles/exercicio1';
+import Card from './components/exercicio2';
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Botao>Start</Botao>
+      <Card>
+        <spam>Texto</spam>
+      </Card>
       <p />
     </div>
   );
---
+++ styled-components-exercises/src/components/exercicio2.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const Card = styled.div`
+  background-color: #f2f2f2;
+  box-shadow: 0 0 40px rgba(0, 200, 200, 1);
+  padding: 20px;
+  width: 300px;
+  height: 200px;
+  margin-left: 20px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export default Card;
