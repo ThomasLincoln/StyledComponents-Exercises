3e6ce07dbad23d07fcabd27a7d77c602d2ea4bcb
Thomas Lincoln <thmas5952@gmail.com>
2023-07-13 18:30:11 -0300

    Mansonry (exercicio 14)

--- styled-components-exercises/src/styles/exercicio14.js
+++ styled-components-exercises/src/styles/exercicio14.js
@@ -1,74 +1,48 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable react/no-array-index-key */
-/* eslint-disable no-return-assign */
 /* eslint-disable react/prop-types */
-/* eslint-disable jsx-a11y/img-redundant-alt */
-import React, { useState, useEffect, useRef } from 'react';
+import React from 'react';
 import styled from 'styled-components';
+import Masonry from 'react-masonry-css';
 
-const Grid = styled.div`
-  display: flex;
-  justify-content: space-between;
-`;
-
-const Column = styled.div`
-  flex: 0 0 calc(33.33% - 20px);
-  margin-right: 20px;
-
-  &:last-child {
-    margin-right: 0;
-  }
-`;
-
-const Item = styled.div`
-  margin-bottom: 20px;
+const Layout = styled.div`
   img {
-    width: 100%;
-    height: auto;
-    display: block;
+    width: 32vw;
+  }
+  margin: 0px 10px;
+  max-width: 100vw;
+  .my-masonry-grid {
+    display: -ms-flexbox; /* Not needed if autoprefixing */
+    display: flex;
+    margin-left: -30px; /* gutter size offset */
+    width: auto;
+  }
+  .my-masonry-grid_column {
+    width: 10px;
+    padding-left: 30px; /* gutter size */
+    background-clip: padding-box;
+  }
+
+  /* Style your items */
+  .my-masonry-grid_column > div {
+    /* change div to reference your elements you put in <Masonry> */
+    width: 33.33%;
+    background: grey;
+    margin-bottom: 30px;
   }
 `;
 
 function MasonryGrid({ links }) {
-  const [columns, setColumns] = useState([]);
-
-  useEffect(() => {
-    const createColumns = () => {
-      const colRefs = Array.from({ length: 3 }, () => ({
-        items: [],
-        height: 0,
-      }));
-
-      links.forEach((link, index) => {
-        const img = new Image();
-        img.src = link;
-        img.onload = () => {
-          const shortestCol = colRefs.reduce((shortest, col) =>
-            col.height < shortest.height ? col : shortest
-          );
-          shortestCol.items.push({ link, height: img.height });
-          shortestCol.height += img.height;
-
-          setColumns([...colRefs]);
-        };
-      });
-    };
-
-    createColumns();
-  }, [links]);
-
   return (
-    <Grid>
-      {columns.map((col, colIndex) => (
-        <Column key={colIndex}>
-          {col.items.map((item, itemIndex) => (
-            <Item key={itemIndex} style={{ height: item.height }}>
-              <img src={item.link} alt={`Image ${colIndex}-${itemIndex}`} />
-            </Item>
-          ))}
-        </Column>
-      ))}
-    </Grid>
+    <Layout>
+      <Masonry
+        breakpointCols={3}
+        className="my-masonry-grid"
+        columnClassName="my-masonry-grid_column"
+      >
+        {links.map((item) => (
+          <img src={item} alt="teste" />
+        ))}
+      </Masonry>
+    </Layout>
   );
 }
 
